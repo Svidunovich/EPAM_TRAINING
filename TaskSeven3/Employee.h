709f40b8d878f71eb213bc597071d283a1b3d078
Yaroslav Svidunovich <q77qw2000@gmail.com>
2020-10-16 13:22:23 +0300

    TaskSeven

---
+++ TaskSeven3/Employee.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+class Employee{
+	private: 
+		std::string _name;
+		long _number;
+	public:
+		Employee();
+		void getData();
+		void putData();
+};

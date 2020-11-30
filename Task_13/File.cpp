979512b1488247946983b7a6ea552b515a4c14ff
Yaroslav Svidunovich <q77qw2000@gmail.com>
2020-11-30 09:13:43 +0300

    Task_13

--- Task_13/File.cpp
+++ Task_13/File.cpp
@@ -3,9 +3,11 @@
 File::File(const char* filename)
 {
 	_file.open(filename, std::ios_base::app);
+	std::cout << "file open" << std::endl;
 }
 
 File::~File()
 {
 	_file.close();
+	std::cout << "file close" << std::endl;
 }
